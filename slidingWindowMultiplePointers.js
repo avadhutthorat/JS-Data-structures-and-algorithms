2cd8c4b408d73e12816bc8347e2e6805db883320
Avadhut Thorat <avadhutthorat40@gmail.com>
2020-02-01 10:00:24 +0530

    multiple pointers pattern
    multiple pointers pattern - sliding window solution

---
+++ slidingWindowMultiplePointers.js
@@ -0,0 +1,15 @@
+function subArraySlidingWindow(arr,num){
+let tempSum = 0, maxSum=0;
+	if(arr.length < num) return null;
+	for(let i=0 ; i< num; i++){
+		tempSum += arr[i]
+	}
+	maxSum= tempSum;
+  for(let i=num; i< arr.length; i++){
+  	tempSum = tempSum - arr[i-num] + arr[i];
+    maxSum = Math.max(tempSum, maxSum)
+  }
+  return maxSum
+}
+
+console.log(subArraySlidingWindow([2,6,9,2,1,8,5,6,13],4))
