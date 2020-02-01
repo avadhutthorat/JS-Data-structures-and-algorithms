ff125db7eebd4565174995ad424af17dac0acd98
Avadhut Thorat <avadhutthorat40@gmail.com>
2020-02-01 17:17:52 +0530

    Create averagepair.js

---
+++ MultiplePointer/averagepair.js
@@ -0,0 +1,22 @@
+function averagePair(arr, target){
+	if(arr.length === 0 ) return false
+	let i=0;
+  let j= arr.length -1;
+  while(i<j){
+  	let avg = (arr[i] + arr[j]) / 2
+    console.log(avg)
+  	if(avg < target){
+    		i++;
+    } else if(avg > target){
+    		j--
+    } else if(avg === target){
+    	return true
+    }
+  }
+  return false
+}
+
+console.log(averagePair([1,3,3,5,6,7,10,12,19],5.5))
+
+// time - O(n)
+// space - O(1)
